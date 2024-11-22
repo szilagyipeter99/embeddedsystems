ec7d91126d6e457239eb236f780b4378cb99fdad
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2024-11-22 17:58:18 +0100

    Update main.ino

--- 02-Peripherals/01-Debounce/main.ino
+++ 02-Peripherals/01-Debounce/main.ino
@@ -4,22 +4,30 @@
 uint8_t ledState = 0;
 
 void setup() {
-    
-    pinMode(LED_PIN, OUTPUT);
-    pinMode(BTN_PIN,INPUT_PULLUP);
 
-    digitalWrite(LED_PIN, 0);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT_PULLUP);
+
+  digitalWrite(LED_PIN, 0);
 }
 
 void loop() {
 
-    if (!digitalRead(BTN_PIN)) {  
-        delay(50);            
-        if (!digitalRead(BTN_PIN)) { 
-            ledState = ledState ^ 1; 
-            digitalWrite(LED_PIN, ledState);
-            while (!digitalRead(BUTTON)); 
-        }
+  /*
+  if (!digitalRead(BTN_PIN)) {
+    ledState = ledState ^ 1;
+    digitalWrite(LED_PIN, ledState);
+  }
+  */
+
+  if (!digitalRead(BTN_PIN)) {
+    delay(50);
+    if (!digitalRead(BTN_PIN)) {
+      ledState = ledState ^ 1;
+      digitalWrite(LED_PIN, ledState);
+      while (!digitalRead(BTN_PIN));
     }
-    
+  }
+
+  delay(1);
 }
