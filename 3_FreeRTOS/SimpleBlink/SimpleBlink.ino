ae39f21aec3523ac9f09bb10001441122faee623
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-03-03 20:39:56 +0100

    Update SimpleBlink.ino

--- 3_FreeRTOS/SimpleBlink/SimpleBlink.ino
+++ 3_FreeRTOS/SimpleBlink/SimpleBlink.ino
@@ -1,48 +1,44 @@
-#define LED1 25
-#define LED2 26
-#define LED3 17
+#define LED_1_PIN 4
+#define LED_2_PIN 5
 
 void setup() {
-    pinMode(LED3, OUTPUT);
-  
-    xTaskCreate(
-        blink1,      // Function name of the task
-        "Blink 1",   // Name of the task (e.g. for debugging)
-        2048,        // Stack size (bytes)
-        NULL,        // Parameter to pass
-        1,           // Task priority
-        NULL         // Task handle
-    );
-    xTaskCreate(
-        blink2,     // Function name of the task
-        "Blink 2",  // Name of the task (e.g. for debugging)
-        2048,       // Stack size (bytes)
-        NULL,       // Parameter to pass
-        1,          // Task priority
-        NULL        // Task handle
-    );
+
+  Serial.begin(115200);
+  Serial.println("Starting...");
+  Serial.println(portTICK_PERIOD_MS);
+  Serial.println(configTICK_RATE_HZ);
+
+  pinMode(LED_1_PIN, OUTPUT);
+  pinMode(LED_2_PIN, OUTPUT);
+
+  xTaskCreate(
+    blinkLED1,   // Function name of the task, '&blinkLed1' would also work
+    "Blink #1",  // Name of the task (e.g. for debugging)
+    2048,        // Stack size (in bytes)
+    NULL,        // Parameter to pass (NULL: a pointer to nowhere)
+    1,           // Task priority
+    NULL         // Task handle to interact with it
+  );
+
+  xTaskCreate(blinkLED2, "Blink #2", 2048, NULL, 1, NULL);
 }
-void blink1(void *parameter) {
-    pinMode(LED1, OUTPUT);
-    while(1){
-        digitalWrite(LED1, HIGH);
-        delay(500); // Delay for Tasks 
-        digitalWrite(LED1, LOW);
-        delay(500);
-    }
+
+void blinkLED1(void *param) {
+  while (1) {
+    digitalWrite(LED_1_PIN, 1);
+    vTaskDelay(500 / portTICK_PERIOD_MS);  // Same as 'delay(500);' on an ESP32
+    digitalWrite(LED_1_PIN, 0);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
-void blink2(void *parameter) {
-    pinMode(LED2, OUTPUT);
-    while(1) {
-        digitalWrite(LED2, HIGH);
-        delay(333);
-        digitalWrite(LED2, LOW);
-        delay(333);
-    }
+
+void blinkLED2(void *param) {
+  while (1) {
+    digitalWrite(LED_2_PIN, 1);
+    vTaskDelay(125 / portTICK_PERIOD_MS);
+    digitalWrite(LED_2_PIN, 0);
+    vTaskDelay(125 / portTICK_PERIOD_MS);
+  }
 }
-void loop(){
-    digitalWrite(LED3, HIGH);
-    delay(1111);
-    digitalWrite(LED3, LOW);
-    delay(1111);
-}
+
+void loop() {}
