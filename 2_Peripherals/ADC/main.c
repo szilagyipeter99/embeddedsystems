9af8f94d4566ee63589005c84a38ade94f245128
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-05-21 14:59:29 +0200

    Update main.c

--- 2_Peripherals/ADC/main.c
+++ 2_Peripherals/ADC/main.c
@@ -17,7 +17,7 @@ void app_main(void) {
 
 	- This example installs an ADC instance for oneshot mode
 
-	- Info about continous mode:
+	- Info about continuous mode:
 	  https://docs.espressif.com/projects/esp-idf/en/stable/esp32c6/api-reference/peripherals/adc_continuous.html#driver-concepts
 
 	*/
