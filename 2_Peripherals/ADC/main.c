76b24cf64cdb63af06a54f8b9baecf8622952205
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-05-21 11:55:56 +0200

    Update main.c

--- 2_Peripherals/ADC/main.c
+++ 2_Peripherals/ADC/main.c
@@ -5,41 +5,76 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-static int adc_raw_measurement;
-static int adc_calibrated_voltage;
+#define MEAS_PIN ADC_CHANNEL_3 // GPIO 3
+
+// These variables will always hold the latest values
+static int raw_meas;
+static int cali_voltage;
 
 void app_main(void) {
 
+	/*
+
+	- This example installs an ADC instance for oneshot mode
+
+	- Info about continous mode:
+	  https://docs.espressif.com/projects/esp-idf/en/stable/esp32c6/api-reference/peripherals/adc_continuous.html#driver-concepts
+
+	*/
+
+	// Allocate a handle for the ADC instance
 	adc_oneshot_unit_handle_t adc_handle;
 
+	// Unit configuration for the initialization process
 	adc_oneshot_unit_init_cfg_t init_config = {
 		.unit_id = ADC_UNIT_1,
 		.clk_src = ADC_DIGI_CLK_SRC_DEFAULT,
 		.ulp_mode = ADC_ULP_MODE_DISABLE,
 	};
-	adc_oneshot_new_unit(&init_config, &adc_handle);
 
+	// Channel configuration
 	adc_oneshot_chan_cfg_t input_config = {
-		.atten = ADC_ATTEN_DB_12,
-		.bitwidth = ADC_BITWIDTH_12,
+		.atten = ADC_ATTEN_DB_12,	 // 12 dB attenuation (divide signal by ~4)
+		.bitwidth = ADC_BITWIDTH_12, // 12-bit resolution
 	};
-	adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_3, &input_config);
 
+	adc_oneshot_new_unit(&init_config, &adc_handle);
+	adc_oneshot_config_channel(adc_handle, MEAS_PIN, &input_config);
+
+	/*
+
+	- It is possible to convert the raw ADC measurement value to the real
+	  measured voltage. By design, the ADC reference voltage for  ESP32C6 is
+	  1100 mV, but in reality it ranges from 1000 mV to 1200 mV. Using the
+	  calibration driver the output results will be more accurate.
+
+	- ESP32C6 supports the 'ADC_CALI_SCHEME_VER_CURVE_FITTING' scheme.
+
+	- The ADC is sensitive to noise, a bypass capacitor (like a 100 nF ceramic)
+	  may need to be connected to the ADC input to minimize noise. To further
+	  mitigate the effects of noise multisampling may be used.
+
+	*/
+
+	// Calibration handle
 	adc_cali_handle_t cali_handle = NULL;
 
+	// Configure a Curve Fitting calibration scheme
 	adc_cali_curve_fitting_config_t cali_config = {
 		.unit_id = ADC_UNIT_1,
-		.chan = ADC_CHANNEL_3, // GPIO (3)
+		.chan = MEAS_PIN,
 		.atten = ADC_ATTEN_DB_12,
 		.bitwidth = ADC_BITWIDTH_12,
 	};
+
 	adc_cali_create_scheme_curve_fitting(&cali_config, &cali_handle);
 
 	while (true) {
-		adc_oneshot_read(adc_handle, ADC_CHANNEL_3, &adc_raw_measurement);
-		adc_cali_raw_to_voltage(cali_handle, adc_raw_measurement,
-								&adc_calibrated_voltage);
-		ESP_LOGI("Main", "Voltage: %d mV (Raw: %d)", adc_calibrated_voltage, adc_raw_measurement);
+		// Perform a measurement and write it into 'raw_meas'
+		adc_oneshot_read(adc_handle, MEAS_PIN, &raw_meas);
+		// Convert the result to a voltage and write it into 'cali_voltage'
+		adc_cali_raw_to_voltage(cali_handle, raw_meas, &cali_voltage);
+		ESP_LOGI("Main", "Voltage: %d mV (Raw: %d)", cali_voltage, raw_meas);
 		sleep(1);
 	}
 }
