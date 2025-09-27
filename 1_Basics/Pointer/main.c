4d86c8de4565ce1dd60ba3dde4b9e00db8da84a3
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-09-27 10:47:58 +0200

    Update main.c

--- 1_Basics/Pointer/main.c
+++ 1_Basics/Pointer/main.c
@@ -5,15 +5,18 @@
 
 static const char *TAG = "Main";
 
+// Write a new value to the received memory address
 void give_random_value(uint8_t *val_ptr) { 
 	*val_ptr = esp_random() % 256; 
 }
 
 void app_main(void) {
 
+	// Create the variable
 	uint8_t some_value = 0;
 
 	while (true) {
+		// The input is the memory address of the variable
 		give_random_value(&some_value);
 		ESP_LOGI(TAG, "New value: %d", some_value);
 		sleep(1);
