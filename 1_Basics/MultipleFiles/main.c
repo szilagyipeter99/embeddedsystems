04581771a85a30f9b44dd49ea8a6c0a540813c80
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-09-27 10:42:55 +0200

    Update main.c

--- 1_Basics/MultipleFiles/main.c
+++ 1_Basics/MultipleFiles/main.c
@@ -1,22 +1,23 @@
 #include "esp_log.h"
-#include "temp_monitor.h"
 #include <stdbool.h>
 #include <unistd.h>
+// Include the custom header file
+#include "temp_monitor.h"
 
-#define COLD_THRESHOLD 20.0
+#define COLD_THRESHOLD 20.0f
 
 static const char *TAG = "Main";
 
 void app_main(void) {
-	
+
 	ESP_LOGI(TAG, "----------------------------");
 
 	while (true) {
-		float data = readTemperature();
-		ESP_LOGI(TAG, "Sensor data: %.2f", data);
-		int8_t roundedTemp = getRoundedTemperature(data);
-		ESP_LOGI(TAG, "Rounded temperature: %d °C", roundedTemp);
-		evaluateTemperature(data, COLD_THRESHOLD);
+		float temp_from_sensor = read_temperature();
+		ESP_LOGI(TAG, "Sensor data: %.2f", temp_from_sensor);
+		int8_t rounded_temp = get_rounded_temperature(temp_from_sensor);
+		ESP_LOGI(TAG, "Rounded temperature: %d °C", rounded_temp);
+		evaluate_temperature(temp_from_sensor, COLD_THRESHOLD);
 		ESP_LOGI(TAG, "----------------------------");
 		sleep(2);
 	}
