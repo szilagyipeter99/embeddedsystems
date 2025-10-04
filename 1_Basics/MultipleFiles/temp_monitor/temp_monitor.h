1f12a04232889fdb88c66504f744d276ac74bd21
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-10-04 21:33:09 +0200

    Update temp_monitor.h

--- 1_Basics/MultipleFiles/temp_monitor/temp_monitor.h
+++ 1_Basics/MultipleFiles/temp_monitor/temp_monitor.h
@@ -3,10 +3,6 @@
 #ifndef TEMP_MONITOR_H
 #define TEMP_MONITOR_H
 
-// Include the following header to use fixed-width integer types like 'int8_t'
-// (was automatically included in 'main.c')
-#include <stdint.h>
-
 float read_temperature();
 
 int8_t get_rounded_temperature(float temperature);
