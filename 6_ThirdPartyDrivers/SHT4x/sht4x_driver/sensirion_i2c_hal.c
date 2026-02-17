1d1daa228ec12afe0b2283ba4a8c68784f94e990
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2026-02-17 23:31:55 +0100

    Update sensirion_i2c_hal.c

--- 6_ThirdPartyDrivers/SHT4x/sht4x_driver/sensirion_i2c_hal.c
+++ 6_ThirdPartyDrivers/SHT4x/sht4x_driver/sensirion_i2c_hal.c
@@ -109,11 +109,8 @@ int8_t sensirion_i2c_hal_write(uint8_t address, const uint8_t* data,
  * Sleep for a given number of microseconds. The function should delay the
  * execution for at least the given time, but may also sleep longer.
  *
- * Despite the unit, a <10 millisecond precision is sufficient.
- *
  * @param useconds the sleep time in microseconds
  */
 void sensirion_i2c_hal_sleep_usec(uint32_t useconds) {
-    /* TODO:IMPLEMENT */
-    vTaskDelay(useconds / 1000);
+    vTaskDelay(pdMS_TO_TICKS(useconds / 1000));
 }
